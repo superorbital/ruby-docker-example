d66524878355e007113b2d24626015d9b3241d38
jmcshane <james.mcshane@superorbital.io>
2023-01-18 15:27:38 -0500

    build mounts

--- .buildkite/steps/image.sh
+++ .buildkite/steps/image.sh
@@ -4,10 +4,10 @@ set -euxo pipefail
 
 apk add jq
 
-docker buildx create --name remote --driver remote --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/cert
-s/key.pem tcp://buildkitd.default.svc:1234
-docker buildx use remote
-docker buildx build --tag=ttl.sh/buildkite-ruby-sample:5m --platform=linux/arm64 --push --metadata-file=$(pwd)/metadata.json .
+docker buildx create --name remote --driver remote --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem tcp://buildkitd.default.svc:1234
 
-image=$(cat metadata.json | jq -r '"\(."image.name")@\(."containerimage.digest")"'
+docker buildx use remote
+docker buildx build --tag=ttl.sh/buildkite-ruby-sample:5m --platform=linux/arm64 --push --metadata-file="$(pwd)/metadata.json" .
+
+image=$(jq -r '"\(."image.name")@\(."containerimage.digest")"' metadata.json)
 buildkite-agent metadata set "image" "${image}"
