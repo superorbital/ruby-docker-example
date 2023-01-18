fcc176f85c35c118b6b9adfc8a8defe7a2ab2b9d
jmcshane <james.mcshane@superorbital.io>
2023-01-18 16:18:42 -0500

    Push to ghcr

--- .buildkite/steps/image.sh
+++ .buildkite/steps/image.sh
@@ -7,7 +7,7 @@ apk add jq
 docker buildx create --name remote --driver remote --driver-opt cacert=/buildkit/certs/ca.pem,cert=/buildkit/certs/cert.pem,key=/buildkit/certs/key.pem tcp://buildkitd.default.svc:1234
 
 docker buildx use remote
-docker buildx build --tag=ttl.sh/buildkite-ruby-sample:5m --platform=linux/arm64 --push --metadata-file="$(pwd)/metadata.json" .
+HOME=/creds docker buildx build --tag="ghcr.io/superorbital/ruby-docker-example:${BUILDKITE_COMMIT}" --platform=linux/arm64 --push --metadata-file="$(pwd)/metadata.json" .
 
 image=$(jq -r '"\(."image.name")@\(."containerimage.digest")"' metadata.json)
 buildkite-agent meta-data set "image" "${image}"
--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM ruby:2.7-alpine
 
+LABEL org.opencontainers.image.source = "https://github.com/superorbital/ruby-docker-example"
+
 WORKDIR /app
 
 ADD Gemfile Gemfile.lock /app/
